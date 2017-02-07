405d0ff973f57b196de87b1dfd584fa280250e0d
linxq <lxq@weetgo.com>
2017-02-07 12:48:09 +0800

    my first test.

---
+++ mycppexam/main.cpp
@@ -0,0 +1,24 @@
+/**
+* 用于我的C++知识点练习.
+* @email: linxq@liv.com
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(int argc, char* argv[]) {
+	std::vector<int> vec = { 1, 2, 3 };
+
+	// test lambda
+	std::transform(vec.begin(), vec.end(), vec.begin(),
+		[](int v) {
+		return v + 1;
+	});
+
+	for (int e : vec) {
+		std::cout << e << std::endl;
+	}
+
+	return 0;
+}
